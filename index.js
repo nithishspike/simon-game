1d75a28c67b85488f0a83378cc394d165b30131f
nithishspike <120673271+nithishspike@users.noreply.github.com>
2023-08-10 08:10:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+
+var l = ["red", "green", "blue", "yellow"];
+    // Generate a random color
+var  r=[];
+j=0;
+function get_ready(){
+  getready(1);
+}
+$(document).keypress(async function(e)
+  { 
+    if(e.key=="Enter")
+    {
+      $("h1").text("Round 1");
+     getready(1);
+    }
+  });
+async function getready(n)
+{
+     for(var i=0;i<n;i++)
+      {
+        var rand = l[Math.floor(Math.random() * 4)]; 
+     
+        var d = new Audio();
+        d.src = "sounds/"+rand+".mp3"; // Set the audio source to blue.mp3
+        d.play(); // Play the audio
+        $("." + rand).addClass("pressed");
+        r.push(rand);
+        await sleep(1);
+        $("." + rand).removeClass("pressed");  
+        await sleep(0.2);
+      }
+}
+  console.log(r);
+  async function sleep(sec)
+  {
+      return new Promise(function(resolve) { setTimeout(resolve,sec*1000)});
+  }
+  $(".btn").click( 
+  async function()
+ {
+    var v=this.classList[1];
+    $("."+v).addClass("pressed");
+    var d=new Audio();
+    d.src="sounds/"+v+".mp3";
+    d.play();
+    await sleep(0.2);
+    $("."+v).removeClass("pressed");
+    if (v!=r[j]){
+    alert("game over");
+    d=0;
+    }
+    else{
+    j++;
+    if(j==r.length)
+    {
+      $("h1").text("Round "+(j+1));
+      var o=r.length;
+      r=[];
+      j=0;
+      await sleep(0.6);
+      getready(o+1);
+
+    }
+        }
+    console.log(this.classList[1]);
+
+ });
+$(".reset").click(get_ready);
+ //$(".btn").click(function(){$("h1").fadeToggle();
+//});
+$(document).ready(() => {
+  const lheight = $('.element').height();
+  setInterval(() => {
+      $('h1').fadeIn(700);
+      $('h1').fadeOut(700);
+
+      // The following code keeps the 
+      // height of the div intact
+      if ($('.element').height() !== 0) {
+          $('.element').css('height', `${lheight}px`);
+      }
+  }, 500);
+});
